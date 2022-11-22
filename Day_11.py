18a2e31698d4f67a4441e1dc60e4e62966ac3114
Colin Martin <colin.martin@gmail.com>
2022-11-21 21:23:37 -0500

    Updates

--- Day_11.py
+++ Day_11.py
@@ -11,9 +11,6 @@ class Octopus:
     neighbors: list["Octopus"] = field(default_factory=list, repr=False)
     has_flashed: bool = False
 
-    def __post_init__(self):
-        pass
-
     def step(self):
         self.value += 1
 
@@ -26,6 +23,8 @@ class Octopus:
     def flash(self):
         if self.value > 9:
             self.has_flashed = True
+            global flash_counter
+            flash_counter += 1
 
     def clean_up(self):
         if self.value > 9:
@@ -33,8 +32,15 @@ class Octopus:
         self.has_flashed = False
 
     def generate_neighbors(self):
-        all_neighbors = [octo for octo in octo_list if octo.x in (self.x + 1, self.x, self.x - 1) and octo.y in (self.y + 1, self.y, self.y - 1)]
-        original = [octo for octo in all_neighbors if octo.x == self.x and octo.y == self.y]
+        all_neighbors = [
+            octo
+            for octo in octo_list
+            if octo.x in (self.x + 1, self.x, self.x - 1)
+               and octo.y in (self.y + 1, self.y, self.y - 1)
+        ]
+        original = [
+            octo for octo in all_neighbors if octo.x == self.x and octo.y == self.y
+        ]
         self.neighbors = [x for x in all_neighbors if x not in original]
 
 
@@ -50,7 +56,6 @@ octo9 = Octopus(value=1, x=-1, y=0)
 octo10 = Octopus(value=1, x=2, y=2)
 
 octo_list = [octo1, octo2, octo3, octo4, octo5, octo6, octo7, octo8, octo9, octo10]
-
 
 for each in octo_list:
     each: Octopus
